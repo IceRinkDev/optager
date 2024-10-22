854dcb98ff340a5b2fcf6e66842a68164cba5a83
IceRinkDev <github@schwarz-ko.de>
2024-10-23 00:00:44 +0200

    ✨ (install): Symlink binaries to ~/.local/bin
    Symlink the binaries and add their names to the pkgs.json

--- cmd/install.go
+++ cmd/install.go
@@ -9,6 +9,7 @@ import (
 	"io/fs"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 
 	"github.com/IceRinkDev/optager/internal/storage"
@@ -18,6 +19,8 @@ import (
 var installCmd = &cobra.Command{
 	Use:   "install PATH-TO-ARCHIVE",
 	Short: "Install an archive into /opt/",
+	Long: `optager install lets you install an archive into /opt/.
+By default it will also symlink the binaries to ~/.local/bin/.`,
 	Args: func(cmd *cobra.Command, args []string) error {
 		err := cobra.ExactArgs(1)(cmd, args)
 		if err != nil {
@@ -37,33 +40,102 @@ var installCmd = &cobra.Command{
 	},
 	Run: func(cmd *cobra.Command, args []string) {
 		output, err := exec.Command("bash", "-c", fmt.Sprintf("tar --exclude=\"*/*\" -tf %s", args[0])).Output()
-		if err == nil {
-			outstr := string(output)
-			folderNames := strings.Split(strings.TrimSpace(outstr), "\n")
-			if len(folderNames) > 1 {
-				fmt.Println("This would extract the following folders and files into /opt/:")
-				for _, folderName := range folderNames {
-					fmt.Println("\t", folderName)
-				}
-				fmt.Println("This archive is most likely not supposed to be installed into /opt/")
-				os.Exit(1)
+		if err != nil {
+			fmt.Println("Error: problem inspecting the archive")
+			os.Exit(1)
+		}
+
+		var newPkg storage.Pkg
+
+		outstr := string(output)
+		folderNames := strings.Split(strings.TrimSpace(outstr), "\n")
+		if len(folderNames) > 1 {
+			fmt.Println("This would extract the following folders and files into /opt/:")
+			for _, folderName := range folderNames {
+				fmt.Println("\t", folderName)
 			}
-			if len(folderNames) == 1 {
-				xdgStorage := storage.New()
-				xdgStorage.AddPkg(storage.Pkg{FolderName: folderNames[0]})
-			} else {
-				fmt.Println("Error: archive is empty")
-				os.Exit(1)
-			}
+			fmt.Println("This archive is most likely not supposed to be installed into /opt/")
+			os.Exit(1)
+		} else if len(folderNames) == 1 {
+			newPkg = storage.Pkg{FolderName: folderNames[0]}
+		} else {
+			fmt.Println("Error: archive is empty")
+			os.Exit(1)
 		}
 
 		err = exec.Command("sudo", "tar", "-xf", args[0], "-C", "/opt/").Run()
 		if err != nil {
 			fmt.Println("Error: could not extract the archive")
+			os.Exit(1)
 		}
+
+		linkedBinaries := symlinkToUser(newPkg.FolderName)
+		if len(linkedBinaries) > 0 {
+			newPkg.Binaries = linkedBinaries
+			fmt.Println("Successfully installed the package")
+			fmt.Print("You can now use ")
+			for i, binary := range linkedBinaries {
+				switch i {
+				case 0:
+					fmt.Printf("\"%s\"", binary)
+				case len(linkedBinaries) - 1:
+					fmt.Printf(" and \"%s\"", binary)
+				default:
+					fmt.Printf(", \"%s\"", binary)
+				}
+			}
+			fmt.Println(" in the command line")
+		} else {
+			fmt.Println("No binaries usable")
+		}
+		xdgStorage := storage.New()
+		xdgStorage.AddPkg(newPkg)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(installCmd)
+}
+
+func symlinkToUser(folder string) []string {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Println("Error: no home folder found")
+		return nil
+	}
+	localBin := filepath.Join(homeDir, ".local", "bin")
+	err = os.MkdirAll(localBin, 0775)
+	if err != nil {
+		fmt.Println("Error: could not create folder", localBin)
+		return nil
+	}
+
+	pkgBinPath := filepath.Join("/opt", folder, "bin")
+	return symlink(pkgBinPath, localBin)
+}
+
+func symlink(srcPath, destPath string) (linkedBinaries []string) {
+	pkgBinDir, err := os.Open(srcPath)
+	if err != nil {
+		if errors.Is(err, fs.ErrNotExist) {
+			fmt.Println("Error: installed package has no bin/ folder")
+		} else {
+			fmt.Println("Error: could not access", srcPath)
+		}
+		return
+	}
+	binaries, err := pkgBinDir.Readdirnames(0)
+	if err != nil {
+		fmt.Println("Error: could not read the contents of the", srcPath, "folder")
+		return
+	}
+	for _, binary := range binaries {
+		err := os.Symlink(filepath.Join(srcPath, binary), filepath.Join(destPath, binary))
+		if err != nil {
+			fmt.Println("Error: could not create symlink for binary")
+		} else {
+			linkedBinaries = append(linkedBinaries, binary)
+		}
+	}
+	return
 }
